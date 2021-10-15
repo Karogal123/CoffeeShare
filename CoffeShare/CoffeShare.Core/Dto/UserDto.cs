53862d969ee025d3075b1efeaa4905f74ec65976
Kamil Rogal <krogalinski@fis-sst.pl>
2021-10-15 14:00:52 +0200

    Dtos added

---
+++ CoffeShare/CoffeShare.Core/Dto/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShare.Core.Dto
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public DateTime JoinedDate { get; set; } = DateTime.Now;
+    }
+}
