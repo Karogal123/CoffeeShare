32875c349da77e4b056ec20891db6fdd4027ffe6
Kamil Rogaliński <krogalinski@fis-sst.pl>
2021-10-19 14:52:10 +0200

    CoffeesController added

---
+++ CoffeShare/CoffeShare/Controllers/CoffeesController.cs
@@ -0,0 +1,25 @@
+﻿using CoffeeShare.Core.Dto;
+using CoffeeShare.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CoffeeShare.Controllers
+{
+    [ApiController]
+    [Route("Coffee")]
+    public class CoffeesController : ControllerBase
+    {
+        private readonly ICoffeeService _coffeeService;
+        public CoffeesController(ICoffeeService coffeeService)
+        {
+            _coffeeService = coffeeService;
+        }
+
+        [HttpGet]
+        public async Task<List<CoffeeDto>> GetAllCoffees()
+        {
+            var coffees = _coffeeService.GetAllCoffees();
+        }
+    }
+}
