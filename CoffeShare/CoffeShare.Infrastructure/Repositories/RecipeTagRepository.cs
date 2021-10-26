2e29b9aef529c8d946042ec5fb174439744e3e93
KRoglinski <krogalinski@fis-sst.pl>
2021-10-26 15:18:11 +0200

    RecipeTagRepository

---
+++ CoffeShare/CoffeShare.Infrastructure/Repositories/RecipeTagRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoffeeShare.Core.Models;
+using CoffeeShare.Infrastructure.DataContext;
+using CoffeeShare.Infrastructure.Repositories.Interfaces;
+
+namespace CoffeeShare.Infrastructure.Repositories
+{
+    public class RecipeTagRepository : IRecipeTagRepository
+    {
+        private readonly CoffeeContext _context;
+
+        public RecipeTagRepository(CoffeeContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<RecipeTag>> GetTagsForRecipe(int recipeId)
+            => await _context.RecipeTags.Where(x => x.RecipeId == recipeId).ToListAsync();
+
+        public async Task<RecipeTag> GetRecipeTagById(int id)
+            => await _context.RecipeTags.SingleOrDefaultAsync(x => x.Id == id);
+
+        public async Task CreateRecipeTag(RecipeTag recipeTag)
+        {
+            await _context.RecipeTags.AddAsync(recipeTag);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateRecipeTag(RecipeTag recipeTag)
+        {
+            _context.RecipeTags.Update(recipeTag);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteRecipeTag(RecipeTag recipeTag)
+        {
+            _context.RecipeTags.Remove(recipeTag);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
